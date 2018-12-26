d0330e3e729098b5e08c6ae97d5e28547616dd04
Vaibhav Sinha <43650684+VaibhavSinha1908@users.noreply.github.com>
2018-12-26 23:44:50 +0530

    Removing the iterative sub folder and adding a new code solution for C# LanguageBasics

---
+++ LanguageBasics/LanguageBasics/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TryingNullConditionalStatments();
+
+            TryingSwitchCases();
+
+
+
+        }
+
+        private static void TryingSwitchCases()
+        {
+            int a = 10;
+            switch(a)
+            {
+                case 10: Console.WriteLine("Print 10");
+                    break;
+                case 11:
+                case 12: Console.WriteLine("Print 12");
+                    break;
+                default: Console.WriteLine("Print Default");
+                    break;
+            }
+        }
+
+        private static void TryingNullConditionalStatments()
+        {
+            StringBuilder sb = new StringBuilder();
+            string str = null;
+
+            //Elvis Operator or Null Conditional Operator [New Feature in C# 6.0]
+            string s = sb?.ToString();
+
+            //Null Coalescing Operator : Returns the right side value when the left side is null;
+            str = str ?? "Honey I am not home";
+
+            Console.WriteLine(str);
+        }
+    }
+}
