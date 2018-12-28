00399f3a6a85dd5f30fdc403ccccd789190a3513
Vaibhav Sinha <43650684+VaibhavSinha1908@users.noreply.github.com>
2018-12-28 07:18:30 +0530

    Adding new switch case conditions

--- LanguageBasics/LanguageBasics/Program.cs
+++ LanguageBasics/LanguageBasics/Program.cs
@@ -10,12 +10,47 @@ namespace LanguageBasics
     {
         static void Main(string[] args)
         {
-            TryingNullConditionalStatments();
+            //TryingNullConditionalStatments();
 
-            TryingSwitchCases();
+            //TryingSwitchCases();
 
+            int a=10;
+           // TryingSwitchCasesWithUnReachableCaseStmts(a);
 
+            TryingSwitchCasesWithUnReachableCaseStmts_v2(a);
 
+        }
+
+        private static void TryingSwitchCasesWithUnReachableCaseStmts_v2(int a)
+        {
+            switch (a)
+            {
+                case int x:
+                    Console.WriteLine("This works as well");
+                    break;
+                case int b when b > 2: Console.WriteLine("this works too."); //A compile time error: The switch case has already been handled by a previous case.
+                    break;
+                
+
+            }
+        }
+
+        private static void TryingSwitchCasesWithUnReachableCaseStmts(object x)
+        {
+            ///In case there are case statements which are not reachable because of more generic case statements preceeding it, 
+            ///the compiler will issue compile time error.
+            
+            switch (x) {
+                //case object z:
+                //    Console.WriteLine("This is just an object");        //This doesn't compile.
+                //    break; 
+                case int a: Console.WriteLine("This is int");
+                    break;
+                case string s: Console.WriteLine("This is string");
+                    break;
+                case object z: Console.WriteLine("This is just an object");         //This works because the case int executes before this.
+                    break;  
+            }
         }
 
         private static void TryingSwitchCases()
